b2974ccd4612fee4821c03ecfd0dedd813f113ec
Marcelo da Silva Cruz <mcruz@uesc.br>
2023-12-01 17:38:07 -0300

    segundo commit incluindo gerenciar treinandores

--- .NET-P004/Program.cs
+++ .NET-P004/Program.cs
@@ -41,3 +41,41 @@ class Programa
             }
         }
     }
+
+static void GerenciarTreinadores(Academia academia)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("==== Menu Treinadores ====");
+            Console.WriteLine("\n1. Adicionar Treinador");
+            Console.WriteLine("2. Remover Treinador");
+            Console.WriteLine("3. Listar Treinadores");
+            Console.WriteLine("0. Voltar");
+
+            Console.Write("\nEscolha uma opção: ");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    AdicionarTreinador(academia);
+                    break;
+                case "2":
+                    RemoverTreinador(academia);
+                    break;
+                case "3":
+                    ListarTreinadores(academia);
+                    break;
+                case "0":
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+
+
+}
