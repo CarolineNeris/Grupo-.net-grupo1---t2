f1fad4fd4b1afae7d389ff73d6c23e69482199fb
FelipeGregue <felipe_gregue@hotmail.com>
2023-12-05 16:48:19 -0300

    pagamento

--- Grupo-.net7-grupo1---t2/avaliacao/Pagamentos.cs
+++ Grupo-.net7-grupo1---t2/avaliacao/Pagamentos.cs
@@ -1,19 +1,26 @@
-using InterfacePagamento; 
-using Avaliacao; 
+using System; 
+using InterfacePagamento;
+using Avaliacao;
+
 namespace Pagamentos
 {
     public class Pagamento : IPagamento
     {
-        private readonly double valorPlano; 
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public double Desconto { get; set; }
+        public DateTime DataHora { get; set; }
 
-        public Pagamento(double valor)
+        public Pagamento(string descricao, double valor, double desconto, DateTime dataHora)
         {
-            valorPlano = valor; 
+            Descricao = descricao;
+            ValorPorMes = valor;
+            Desconto = desconto;
+            DataHora = dataHora;
         }
 
         public void RealizarPagamentoPlano(double valor)
         {
-            
             Console.WriteLine($"Pagamento realizado no valor de R$ {valor}");
         }
     }
